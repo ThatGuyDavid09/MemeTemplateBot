b542247f36bc590b05624f9711a87cc3e27904ef
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-07-24 14:44:13 -0400

    did some boilerplate for reddit bot

--- template-finder.py
+++ template-finder.py
@@ -1,4 +1,5 @@
 import praw
+import requests
 
 r = praw.Reddit("Meme template finder bot by reddit-be0cool v 1.0.0"
                 "Url: https://thatguydavid09.github.io/redirect/")
@@ -10,3 +11,9 @@ while True:
     # TODO: implement the bot and the ai
     # Bot should activate if its name is called
     # Should take image, put it through api, and make a comment
+
+    if bot_is_summoned:
+    # Get image url
+    resp = requests.get(url="The reddit posts url")
+
+    url = resp.json['data']['children'][0]['data']['url']
