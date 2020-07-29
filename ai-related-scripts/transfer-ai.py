9edb487ccb4072bbe37d0631cdab8cd705180cdd
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-07-29 14:50:23 -0400

    Added description of transfer-ai script

---
+++ ai-related-scripts/transfer-ai.py
@@ -0,0 +1,4 @@
+# This is (or will be) an implementation of a transfer ai that
+# Matches similar memes together and returns some useful info about them
+# This will allow for more obscure memes to be recognized
+# TODO: Actually implement this
