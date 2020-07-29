107481019a454714ef523442399c0e47949c4660
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-07-29 18:49:58 -0400

    Discarded idea

--- ai-related-scripts/transfer-ai.py
+++ ai-related-scripts/transfer-ai.py
@@ -2,11 +2,4 @@
 # Matches similar memes together and returns some useful info about them
 # This will allow for more obscure memes to be recognized
 
-# After some googling, I figured out we might be able to use an image hash to figure out if the images are similar
-# Run pip install imagehash
-from PIL import Image
-import imagehash
-hash1 = imagehash.whash(Image.open('test1.jpg'))
-print(hash1)
-hash2 = imagehash.whash(Image.open('test2.jpg'))
-print(hash2)
+# Hashing isn't going to work, the images can be too different
