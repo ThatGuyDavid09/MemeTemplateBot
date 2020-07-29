cc4757a867ce844f37e56821e54ee4cd3700396b
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-07-29 19:06:42 -0400

    Tried implementation of different alogirthm (Doesn't work, see todo)

--- ai-related-scripts/transfer-ai.py
+++ ai-related-scripts/transfer-ai.py
@@ -3,3 +3,61 @@
 # This will allow for more obscure memes to be recognized
 
 # Hashing isn't going to work, the images can be too different
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
+def compare_images(imageA, imageB, title):
+	# compute the mean squared error and structural similarity
+	# index for the images
+	m = mse(imageA, imageB)
+	s = ssim(imageA, imageB)
+	# setup the figure
+	fig = plt.figure(title)
+	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+	# show first image
+	ax = fig.add_subplot(1, 2, 1)
+	plt.imshow(imageA, cmap = plt.cm.gray)
+	plt.axis("off")
+	# show the second image
+	ax = fig.add_subplot(1, 2, 2)
+	plt.imshow(imageB, cmap = plt.cm.gray)
+	plt.axis("off")
+	# show the images
+	plt.show()
+
+meme = cv2.imread("test1.jpg")
+template = cv2.imread("test2.jpg")
+meme_shape = meme.shape[:2]
+template = cv2.resize(template, meme_shape)
+# convert the images to grayscale
+meme = cv2.cvtColor(meme, cv2.COLOR_BGR2GRAY)
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+# TODO: Make the images the same size
+
+# initialize the figure
+fig = plt.figure("Images")
+images = ("Meme", meme), ("Template", template)
+# loop over the images
+for (i, (name, image)) in enumerate(images):
+	# show the image
+	ax = fig.add_subplot(1, 2, i + 1)
+	ax.set_title(name)
+	plt.imshow(image, cmap = plt.cm.gray)
+	plt.axis("off")
+# show the figure
+plt.show()
+# compare the images
+compare_images(meme, template, "Meme vs. Template")
