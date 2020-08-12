0ba7a8e51b3859a5ddbf02776a6fcfd1eaad4c1c
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-08-12 13:01:17 -0400

    Made transfer-ai importable kind of

--- ai-related-scripts/transfer-ai.py
+++ ai-related-scripts/transfer-ai.py
@@ -2,77 +2,84 @@
 # Matches similar memes together and returns some useful info about them
 # This will allow for more obscure memes to be recognized
 
+import cv2
+import numpy as np
+
 """
 To use:
 Put your 2 images you want to compare in the folder this is in
 Put their file names in the variables below
 """
 
-template = "test1.jpg"
-meme = "test2.jpg"
+template_use = "test1.jpg"
+meme_use = "test2.jpg"
 
-import cv2
-import numpy as np
+def main(template, meme):
+    print(check_match(template, meme))
 
-original = cv2.imread(template)
-image_to_compare = cv2.imread(meme)
+def check_match(template, meme):
+    original = cv2.imread(template)
+    image_to_compare = cv2.imread(meme)
 
 
-# 1) Check if 2 images are equals
-if original.shape == image_to_compare.shape:
-    print("The images have same size and channels")
-    difference = cv2.subtract(original, image_to_compare)
-    b, g, r = cv2.split(difference)
+    # 1) Check if 2 images are equals
+    if original.shape == image_to_compare.shape:
+        return "The images have same size and channels"
+        difference = cv2.subtract(original, image_to_compare)
+        b, g, r = cv2.split(difference)
 
-    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-        print("The images are completely Equal")
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            return "The images are completely Equal"
+        else:
+            return "The images are NOT equal"
+
+    # 2) Check for similarities between the 2 images
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp_1, desc_1 = sift.detectAndCompute(original, None)
+    kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+
+    index_params = dict(algorithm=0, trees=5)
+    search_params = dict()
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(desc_1, desc_2, k=2)
+
+    good_points = []
+    for m, n in matches:
+        if m.distance < 0.6*n.distance:
+            good_points.append(m)
+
+    # Define how similar they are
+    number_keypoints = 0
+    if len(kp_1) <= len(kp_2):
+        number_keypoints = len(kp_1)
     else:
-        print("The images are NOT equal")
+        number_keypoints = len(kp_2)
 
-# 2) Check for similarities between the 2 images
-sift = cv2.xfeatures2d.SIFT_create()
-kp_1, desc_1 = sift.detectAndCompute(original, None)
-kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+        # TODO make these return and not print
+    print("Keypoints 1ST Image: " + str(len(kp_1)))
+    print("Keypoints 2ND Image: " + str(len(kp_2)))
+    print("GOOD Matches:", len(good_points))
+    # Multiplied by 1000 to exaggerate any differences
+    print("How good it's the match: ", len(good_points) / number_keypoints * 1000)
 
-index_params = dict(algorithm=0, trees=5)
-search_params = dict()
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-matches = flann.knnMatch(desc_1, desc_2, k=2)
-
-good_points = []
-for m, n in matches:
-    if m.distance < 0.6*n.distance:
-        good_points.append(m)
-
-# Define how similar they are
-number_keypoints = 0
-if len(kp_1) <= len(kp_2):
-    number_keypoints = len(kp_1)
-else:
-    number_keypoints = len(kp_2)
+    # Following code would show the images, I don't care about that
+    """
+    result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
 
 
-print("Keypoints 1ST Image: " + str(len(kp_1)))
-print("Keypoints 2ND Image: " + str(len(kp_2)))
-print("GOOD Matches:", len(good_points))
-# Multiplied by 1000 to exaggerate any differences
-print("How good it's the match: ", len(good_points) / number_keypoints * 1000)
-
-# Following code would show the images, I don't care about that
-"""
-result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
+    cv2.imshow("result", cv2.resize(result, None, fx=0.4, fy=0.4))
+    cv2.imwrite("feature_matching.jpg", result)
 
 
-cv2.imshow("result", cv2.resize(result, None, fx=0.4, fy=0.4))
-cv2.imwrite("feature_matching.jpg", result)
+    cv2.imshow("Original", cv2.resize(original, None, fx=0.4, fy=0.4))
+    cv2.imshow("Duplicate", cv2.resize(image_to_compare, None, fx=0.4, fy=0.4))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    """
 
-
-cv2.imshow("Original", cv2.resize(original, None, fx=0.4, fy=0.4))
-cv2.imshow("Duplicate", cv2.resize(image_to_compare, None, fx=0.4, fy=0.4))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
+if __name__ == '__main__':
+    main(template_use, meme_use)
 
 # Another attempt, may use later
 """
