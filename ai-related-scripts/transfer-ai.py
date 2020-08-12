cce207bbf210fa10dbe140aff94dc450d945ff3a
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2020-08-12 17:35:16 -0400

    Started work on reddit bot

--- ai-related-scripts/transfer-ai.py
+++ ai-related-scripts/transfer-ai.py
@@ -34,47 +34,51 @@ def check_match(template, meme, debug=False):
     """
     original = cv2.imread(template)
     image_to_compare = cv2.imread(meme)
+    try:
+        # Check for similarities between the 2 images
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp_1, desc_1 = sift.detectAndCompute(original, None)
+        kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
 
-    # Check for similarities between the 2 images
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp_1, desc_1 = sift.detectAndCompute(original, None)
-    kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+        index_params = dict(algorithm=0, trees=5)
+        search_params = dict()
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-    index_params = dict(algorithm=0, trees=5)
-    search_params = dict()
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(desc_1, desc_2, k=2)
 
-    matches = flann.knnMatch(desc_1, desc_2, k=2)
+        good_points = []
+        for m, n in matches:
+            if m.distance < 0.6 * n.distance:
+                good_points.append(m)
 
-    good_points = []
-    for m, n in matches:
-        if m.distance < 0.6 * n.distance:
-            good_points.append(m)
+        # Define how similar they are
+        if len(kp_1) <= len(kp_2):
+            number_keypoints = len(kp_1)
+        else:
+            number_keypoints = len(kp_2)
 
-    # Define how similar they are
-    if len(kp_1) <= len(kp_2):
-        number_keypoints = len(kp_1)
-    else:
-        number_keypoints = len(kp_2)
+        # TODO make these return and not print
+        if debug:
+            print("Keypoints 1ST Image: " + str(len(kp_1)))
+            print("Keypoints 2ND Image: " + str(len(kp_2)))
+            print("GOOD Matches:", len(good_points))
+            # Multiplied by 1000 to exaggerate any differences
+            print("How good it's the match: ", len(good_points) / number_keypoints * 1000)
 
-    # TODO make these return and not print
-    if debug:
-        print("Keypoints 1ST Image: " + str(len(kp_1)))
-        print("Keypoints 2ND Image: " + str(len(kp_2)))
-        print("GOOD Matches:", len(good_points))
-        # Multiplied by 1000 to exaggerate any differences
-        print("How good it's the match: ", len(good_points) / number_keypoints * 1000)
+            # Show images
+            result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
 
-        # Show images
-        result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
+            cv2.imshow("result", cv2.resize(result, None, fx=0.4, fy=0.4))
+            cv2.imwrite("feature_matching.jpg", result)
 
-        cv2.imshow("result", cv2.resize(result, None, fx=0.4, fy=0.4))
-        cv2.imwrite("feature_matching.jpg", result)
+            cv2.imshow("Original", cv2.resize(original, None, fx=0.4, fy=0.4))
+            cv2.imshow("Duplicate", cv2.resize(image_to_compare, None, fx=0.4, fy=0.4))
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        cv2.imshow("Original", cv2.resize(original, None, fx=0.4, fy=0.4))
-        cv2.imshow("Duplicate", cv2.resize(image_to_compare, None, fx=0.4, fy=0.4))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        return [0, len(good_points) / number_keypoints * 1000]
+    except Exception as e:
+        return [-1, e]
 
 
 if __name__ == '__main__':
